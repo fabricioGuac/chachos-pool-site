085ce7836d22dc89512aceabb10cb87f992106f9
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-11-21 15:54:56 -0800

    sets up the basic react tailwind project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='bg-teal-400 text-center text-5xl'>Hello</h1>
+    </>
+  )
+}
+
+export default App
