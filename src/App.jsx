698fc2feade6902b1ec974b33734bcbcaec1b762
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-11-22 12:08:59 -0800

    delves in a basic react router structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import { Outlet } from 'react-router-dom';
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className='bg-teal-400 text-center text-5xl'>Hello</h1>
+    <Header />
+      <Outlet />
+    <Footer />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
 import { StrictMode } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import Homepage from './pages/Homepage.jsx';
+import AboutUs from './pages/AboutUs.jsx';
+import Services from './pages/Services.jsx';
+import Testimonials from './pages/Testimonials.jsx';
+import Contact from './pages/Contact.jsx';
+import ErrorPage from './pages/Errorpage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />
+      }, {
+        path:'/about',
+        element: <AboutUs />
+      },{
+        path:'/services',
+        element: <Services /> 
+      }, {
+        path: '/testimonials',
+        element: <Testimonials />
+      },{
+        path: '/contact',
+        element: <Contact />
+      },
+    ]
+  }
+])
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
