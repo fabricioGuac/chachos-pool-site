b5420cf00c091c7408c8087b8ac780d9eaa81d96
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-11-22 13:14:03 -0800

    delves in the error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,8 +1,18 @@
-
 export default function ErrorPage() {
     return (
-        <>
-        <h1>Error page</h1>
-        </>
-    )
+        <div className="bg-slate-950 h-screen flex justify-center items-center text-center">
+            <div className="bg-slate-800 p-8 rounded-lg ">
+                <h1 className="text-4xl text-white font-bold mb-4">404</h1>
+                <p className="text-slate-300 text-lg mb-6">
+                    Oops! The page you're looking for doesn't exist.
+                </p>
+                <a
+                    href="/"
+                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300"
+                >
+                    Go Back to Home
+                </a>
+            </div>
+        </div>
+    );
 }
--- src/pages/Testimonials.jsx
+++ src/pages/Testimonials.jsx
@@ -1,8 +1,17 @@
 
+
 export default function Testimonials() {
     return (
         <>
-        <h1>Testimonials</h1>
+            <h1>Testimonials</h1>
+            <a
+                href="https://www.google.com/maps/place/CHACHOS+POOL+LLC/@28.3372691,-81.6707798,17z/data=!4m8!3m7!1s0x88dd7d86d211eee5:0x3bcef57413186cc!8m2!3d28.3372691!4d-81.6682049!9m1!1b1!16s%2Fg%2F11vwxyflbd?entry=ttu&g_ep=EgoyMDI0MTExOS4yIKXMDSoASAFQAw%3D%3D"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-orange-700 text-white py-2 px-4 rounded-lg hover:bg-orange-800"
+            >
+                Read More Reviews
+            </a>
         </>
     )
 }
